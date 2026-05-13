c07ddb61fbf720008289903814968ac6b97fd609
constantinepyro666 <constantine.pyro666@gmail.com>
2026-05-13 16:30:16 +0500

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS product_prices (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    title TEXT,
+    category TEXT,
+    price NUMERIC,
+    loaded_at TIMESTAMP DEFAULT NOW()
+);
