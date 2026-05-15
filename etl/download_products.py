2e3234f8aeb253e9a8652b828ac22eea50e0412a
constantinepyro666 <constantine.pyro666@gmail.com>
2026-05-15 13:11:25 +0500

    Update download_products.py

--- etl/download_products.py
+++ etl/download_products.py
@@ -1,66 +1,42 @@
 import os
-import time
-import random
 import requests
 import psycopg2
 
 # --- Подключение к PostgreSQL ---
-import time
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            dbname=os.getenv("POSTGRES_DB"),
-            user=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASSWORD"),
-            host="db",
-            port="5432"
-        )
-
-        print("Connected to PostgreSQL")
-        break
-
-    except Exception as e:
-        print("Waiting for PostgreSQL...")
-        print(e)
-        time.sleep(5)
+conn = psycopg2.connect(
+    dbname=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host="db",
+    port="5432"
+)
 
 cur = conn.cursor()
 
-while True:
-    print("Loading products...")
+# --- Получение данных из API ---
+response = requests.get("https://dummyjson.com/products")
+products = response.json()["products"]
 
-    # --- Получение данных ---
-    response = requests.get("https://dummyjson.com/products")
-    products = response.json()["products"]
-
-    # --- Загрузка в БД ---
-    for product in products:
-
-        # Небольшое случайное изменение цены
-        modified_price = round(
-            product["price"] * random.uniform(0.95, 1.05),
-            2
+# --- Загрузка данных в БД ---
+for product in products:
+    cur.execute("""
+        INSERT INTO product_prices (
+            product_id,
+            title,
+            category,
+            price
         )
+        VALUES (%s, %s, %s, %s)
+    """, (
+        product["id"],
+        product["title"],
+        product["category"],
+        product["price"]
+    ))
 
-        cur.execute("""
-            INSERT INTO product_prices (
-                product_id,
-                title,
-                category,
-                price
-            )
-            VALUES (%s, %s, %s, %s)
-        """, (
-            product["id"],
-            product["title"],
-            product["category"],
-            modified_price
-        ))
+conn.commit()
 
-    conn.commit()
+cur.close()
+conn.close()
 
-    print(f"Loaded {len(products)} products")
-
-    # --- Ждем 30 минут ---
-    time.sleep(1800)
+print(f"Loaded {len(products)} products")
